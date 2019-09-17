a7cde8d8da1afb771c9b4a5ebc6e7a8af86f0728
Your Name <mdmujahid97@gmail.com>
2019-09-17 22:04:46 +0530

    It took 2hrs to complete it

---
+++ Chapter8. Conditionals/danger.py
@@ -0,0 +1,20 @@
+words = ["adopt", "bake", "beam", "confide", "grill", "plant", "time", "wave", "wish"]
+past_tense = []
+
+for word in words:
+    if word[-1] == 'e':
+        
+        past_tense.append(word + "d")
+    else :
+        
+        past_tense.append(word + "ed")
+        
+print(past_tense)
+     
+   
+        
+        
+            
+        
+    
+
