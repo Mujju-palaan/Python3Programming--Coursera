1f3efd2ea540b1650918fc94ffe4448a640a6d53
Your Name <mdmujahid97@gmail.com>
2019-09-17 22:05:24 +0530

    It took 2hrs to complete it

--- Chapter8. Conditionals/danger.py
+++ Chapter8. Conditionals/danger.py
@@ -1,3 +1,7 @@
+#Challenge For each word in words, add ‘d’ to the end of the word if the word ends in “e” to make it past tense. Otherwise, add ‘ed’ to make it past tense. Save these past tense words to a list called past_tense.
+
+
+
 words = ["adopt", "bake", "beam", "confide", "grill", "plant", "time", "wave", "wish"]
 past_tense = []
 
