a9cc05820ee617a0e7cf4b779f9030c221cb5039
Your Name <mdmujahid97@gmail.com>
2019-09-26 16:50:18 +0530

    Coursera

---
+++ Chapter14. More About Iteration/even.py
@@ -0,0 +1,9 @@
+eve_nums = []
+i=0
+while i < 15:
+    if ( i % 2==0):
+        print(i)
+        eve_nums.append(i)
+        i+=2
+        
+print(eve_nums)
---
+++ Chapter14. More About Iteration/sum_of_list.py
@@ -0,0 +1,8 @@
+
+list1 = [8, 3, 4, 5, 6, 7, 9]
+accum = 0
+i = 0
+while i < len(list1):
+    accum = accum + list1[i]
+    i = i + 1
+    
