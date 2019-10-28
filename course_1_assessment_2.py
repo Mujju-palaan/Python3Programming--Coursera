f07ba2081d5eccad31a1b2cee615798166dcd97f
tansyDeveloper07- Mujahid <52351431+Mujju-palaan@users.noreply.github.com>
2019-10-28 11:48:15 +0530

    Update course_1_assessment_2.py

--- course_1_assessment_2.py
+++ course_1_assessment_2.py
@@ -1,5 +1,5 @@
 #There is a function we are providing in for you in this problem called square. It takes one integer and returns the square of that integer value. Write code to assign a variable called xyz the value 5*5 (five squared). Use the square function, rather than just multiplying with *.
-xyz = 5
+xyz = 25
 
 squared = square(xyz)
 print(squared)
